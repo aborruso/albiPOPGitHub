80fdf3ffbdf23567a5edd8011c7dc1f95289d867
Andrea Borruso <aborruso@gmail.com>
2020-10-15 22:03:12 +0200

    aggiunta creazione feed

--- c_l736/c_l736.sh
+++ c_l736/c_l736.sh
@@ -4,6 +4,24 @@ set -x
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+### anagrafica albo
+titolo="AlboPOP del comune di Venizia"
+descrizione="L'albo pretorio POP è una versione dell'albo pretorio del tuo comune, che puoi seguire in modo più comodo."
+nomecomune="Venezia"
+webMaster="fabrizio.puce82@gmail.com (Fabrizio Puce)"
+type="Comune"
+municipality="Venezia"
+province="Venezia"
+region="Veneto"
+latitude="45.496144"
+longitude="12.244231"
+country="Italia"
+name="Comune di Venezia"
+uid="istat:027042"
+docs="http://albopop.it/comune/venezia/"
+selflink="http://dev.ondata.it/projs/albopop/patti/feed.xml"
+### anagrafica albo
+
 iPA="c_l736"
 
 mkdir -p "$folder"/rawdata
@@ -29,7 +47,7 @@ if [ $code -eq 200 ]; then
   curl -kL "$URLBase" | jq . >"$folder"/rawdata/albo.json
 
   # converti lista in TSV
-  jq <"$folder"/rawdata/albo.json '.atti[]' | mlr --j2t unsparsify | tail -n +2 | head -n 30 >"$folder"/rawdata/albo.tsv
+  jq <"$folder"/rawdata/albo.json '.atti[]' | mlr --j2t unsparsify | tail -n +2 | head -n 5 >"$folder"/rawdata/albo.tsv
 
   rm "$folder"/rawdata/dettagli.json
 
@@ -41,5 +59,45 @@ if [ $code -eq 200 ]; then
 
   # genera CSV dei dettagli
   mlr <"$folder"/rawdata/dettagli.json --j2c unsparsify >"$folder"/rawdata/dettagli.csv
+
+  mlr --c2t cut -x -r -f "files:[^0].+" \
+    then put -S '$dataInizio = strftime(strptime($dataInizio, "%Y%m%d"),"%a, %d %b %Y %H:%M:%S %z")' "$folder"/rawdata/dettagli.csv | tail -n +2 >"$folder"/rawdata/dettagli.tsv
+
+  mlr --csv cut -x -r -f "files:[^0].+" \
+    then put -S '$dataInizio = strftime(strptime($dataInizio, "%Y%m%d"),"%a, %d %b %Y %H:%M:%S %z")' "$folder"/rawdata/dettagli.csv >"$folder"/rawdata/tmp_dettagli.csv
+
+  # crea copia del template del feed
+  cp "$folder"/../risorse/feedTemplate.xml "$folder"/processing/feed.xml
+
+  # inserisci gli attributi anagrafici nel feed
+  xmlstarlet ed -L --subnode "//channel" --type elem -n title -v "$titolo" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n description -v "$descrizione" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n link -v "$selflink" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n "atom:link" -v "" -i "//*[name()='atom:link']" -t "attr" -n "rel" -v "self" -i "//*[name()='atom:link']" -t "attr" -n "href" -v "$selflink" -i "//*[name()='atom:link']" -t "attr" -n "type" -v "application/rss+xml" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n docs -v "$docs" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$type" -i "//channel/category[1]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-type" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$municipality" -i "//channel/category[2]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-municipality" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$province" -i "//channel/category[3]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-province" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$region" -i "//channel/category[4]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-region" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$latitude" -i "//channel/category[5]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-latitude" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$longitude" -i "//channel/category[6]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-longitude" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$country" -i "//channel/category[7]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-country" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$name" -i "//channel/category[8]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-name" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$uid" -i "//channel/category[9]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-uid" "$folder"/processing/feed.xml
+
+  # leggi in loop i dati del file TSV e usali per creare nuovi item nel file XML
+  newcounter=0
+  while IFS=$'\t' read -r anno numero dataInizio dataFine esibente oggetto sede idDocumento naturaCodice naturaValore tipologiaCodice tipologiaValore path alias nome idEpraxi tipoEpraxi proponenteCodice proponenteValore; do
+    newcounter=$(expr $newcounter + 1)
+    xmlstarlet ed -L --subnode "//channel" --type elem -n item -v "" \
+      --subnode "//item[$newcounter]" --type elem -n title -v "#$naturaValore | Pubblicazione numero $numero" \
+      --subnode "//item[$newcounter]" --type elem -n description -v "$oggetto" \
+      --subnode "//item[$newcounter]" --type elem -n link -v "$path$alias" \
+      --subnode "//item[$newcounter]" --type elem -n pubDate -v "$dataInizio" \
+      --subnode "//item[$newcounter]" --type elem -n guid -v "$path$alias" \
+      "$folder"/processing/feed.xml
+  done <"$folder"/rawdata/dettagli.tsv
+
+  cp "$folder"/processing/feed.xml "$folder"/../docs/"$iPA"
 
 fi
