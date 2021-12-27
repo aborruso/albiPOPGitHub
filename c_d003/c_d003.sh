6431ac539267c432fe00ed68607023e99864b1df
Andrea Borruso <aborruso@gmail.com>
2021-12-27 09:09:42 +0100

    update

--- c_d003/c_d003.sh
+++ c_d003/c_d003.sh
@@ -46,8 +46,8 @@ output="$folder"/../docs/"$iPA"
 URLBase="https://cloud.urbi.it/urbi/progs/urp/ur1ME001.sto?DB_NAME=n1233954"
 
 # estrai codici di risposta HTTP dell'albo
-#code=$(curl -s -L -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0' -o /dev/null -w "%{http_code}" "$URLBase")
-code="200"
+code=$(curl -s -L -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0' -o /dev/null -w "%{http_code}" "$URLBase")
+#code="200"
 
 # se il server risponde fai partire lo script
 if [ $code -eq 200 ]; then
