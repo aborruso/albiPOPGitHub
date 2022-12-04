c9af6e0b2015fafd53fbc04806eefb5efa85ee36
Andrea Borruso <aborruso@gmail.com>
2022-12-04 18:55:37 +0100

    feed comune di Bitonto

---
+++ c_a893/c_a893.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+### requisiti ###
+# mlr https://github.com/johnkerl/miller
+# xmlstarlet http://xmlstar.sourceforge.net/
+# jq https://github.com/stedolan/jq
+### requisiti ###
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+git pull
+
+### anagrafica albo
+titolo="AlboPOP del comune di Bitonto"
+descrizione="L'albo pretorio POP è una versione dell'albo pretorio del tuo comune, che puoi seguire in modo più comodo."
+nomecomune="Bitonto"
+webMaster="aborruso@gmail.com (Andrea Borruso)"
+type="Comune"
+municipality="Bitonto"
+province="Bari"
+region="Pugia"
+latitude="41.10833814167882"
+longitude="16.690904045772836"
+country="Italia"
+name="Comune di Bitonto"
+uid="istat:072011"
+docs="https://albopop.it/comune/bitonto/"
+selflink="https://aborruso.github.io/albiPOPGitHub/c_a893/feed.xml"
+### anagrafica albo
+
+iPA="c_a893"
+
+#http://web11.immediaspa.com/barcellona/mc/mc_p_dettaglio.php?id_pubbl=567&x=&sto=&pag=&mittente=&oggetto=&numero=&tipo_atto=&data_dal=&data_al=&datap_dal=&datap_al=&ordin=&servizio=
+
+# crea cartelle di servizio
+mkdir -p "$folder"/rawdata
+mkdir -p "$folder"/processing
+mkdir -p "$folder"/../docs/"$iPA"
+
+# imposta la cartella di output esposta sul web
+output="$folder"/../docs/"$iPA"
+
+# estrai codici di risposta HTTP dell'albo
+code=$(curl 'https://bitonto.trasparenza-valutazione-merito.it/web/trasparenza/albo-pretorio?p_p_id=jcitygovalbopubblicazioni_WAR_jcitygovalbiportlet&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_jcitygovalbopubblicazioni_WAR_jcitygovalbiportlet_action=eseguiPaginazione' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H 'Accept-Language: en-US,en;q=0.9' -H 'Cache-Control: max-age=0' -H 'Connection: keep-alive' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: null' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' -H 'sec-ch-ua: "Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"' -H 'sec-ch-ua-mobile: ?0' -H 'sec-ch-ua-platform: "Windows"' --data-raw 'hidden_page_size=50&hidden_page_to=' --compressed -o "$folder"/rawdata/pagina.html -w "%{http_code}")
+
+# se il server risponde fai partire lo script
+if [ $code -eq 200 ]; then
+
+  # converti HTML in JSON
+  scrape <"$folder"/rawdata/pagina.html -be '//table[contains(@class, 'master-detail-list-table')]//tr[@data-id]' | xq . >"$folder"/rawdata/pagina.json
+
+  jq <"$folder"/rawdata/pagina.json -c '.html.body.tr[]|{des:.td[3]."#text",id:.td[0]."#text",date:.td[4]."#text",idURL:."@data-id"}' >"$folder"/rawdata/elenco.json
+
+  mlr --j2t clean-whitespace then \
+    put '$date=regextract($date,"^[0-9]{2}/[0-9]{2}/[0-9]{4}");$rssDate = strftime(strptime($date, "%d/%m/%Y"),"%a, %d %b %Y %H:%M:%S %z")' then put '$des=gsub($des,"<","&lt")' \
+    then put '$des=gsub($des,">","&gt;")' \
+    then put '$des=gsub($des,"&","&amp;")' \
+    then put '$des=gsub($des,"'\''","&apos;")' \
+    then put '$des=gsub($des,"\"","&quot;")' \
+    then sort -nr idURL "$folder"/rawdata/elenco.json | tail -n +2>"$folder"/rawdata/albi.tsv
+
+  # crea copia del template del feed
+  cp "$folder"/../risorse/feedTemplate.xml "$folder"/processing/feed.xml
+
+  # inserisci gli attributi anagrafici nel feed
+  xmlstarlet ed -L --subnode "//channel" --type elem -n title -v "$titolo" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n description -v "$descrizione" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n link -v "$selflink" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n "atom:link" -v "" -i "//*[name()='atom:link']" -t "attr" -n "rel" -v "self" -i "//*[name()='atom:link']" -t "attr" -n "href" -v "$selflink" -i "//*[name()='atom:link']" -t "attr" -n "type" -v "application/rss+xml" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n docs -v "$docs" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$type" -i "//channel/category[1]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-type" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$municipality" -i "//channel/category[2]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-municipality" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$province" -i "//channel/category[3]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-province" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$region" -i "//channel/category[4]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-region" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$latitude" -i "//channel/category[5]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-latitude" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$longitude" -i "//channel/category[6]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-longitude" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$country" -i "//channel/category[7]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-country" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$name" -i "//channel/category[8]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-name" "$folder"/processing/feed.xml
+  xmlstarlet ed -L --subnode "//channel" --type elem -n category -v "$uid" -i "//channel/category[9]" -t "attr" -n "domain" -v "http://albopop.it/specs#channel-category-uid" "$folder"/processing/feed.xml
+
+  # leggi in loop i dati del file TSV e usali per creare nuovi item nel file XML
+
+  newcounter=0
+  while IFS=$'\t' read -r des id date idURL rssDate; do
+    URL="https://bitonto.trasparenza-valutazione-merito.it/web/trasparenza/albo-pretorio/-/papca/display/$idURL"
+    newcounter=$(expr $newcounter + 1)
+    xmlstarlet ed -L --subnode "//channel" --type elem -n item -v "" \
+      --subnode "//item[$newcounter]" --type elem -n title -v "$id" \
+      --subnode "//item[$newcounter]" --type elem -n description -v "$des" \
+      --subnode "//item[$newcounter]" --type elem -n link -v "$URL" \
+      --subnode "//item[$newcounter]" --type elem -n pubDate -v "$rssDate" \
+      --subnode "//item[$newcounter]" --type elem -n guid -v "$URL" \
+      "$folder"/processing/feed.xml
+  done <"$folder"/rawdata/albi.tsv
+
+  cp "$folder"/processing/feed.xml "$output"
+
+fi
