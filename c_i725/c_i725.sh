ccec3eef487d86b9900861ee92ebf60710936135
aborruso <aborruso@gmail.com>
2024-05-28 22:56:33 +0200

    update

--- c_i725/c_i725.sh
+++ c_i725/c_i725.sh
@@ -7,4 +7,6 @@ set -o pipefail
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-rsspls -c "$folder"/feeds.toml
+# rsspls -c "$folder"/feeds.toml
+
+google-chrome-stable --headless --disable-gpu --print-to-stdout --virtual-time-budget=10000 --dump-dom "https://siderno.trasparenza-valutazione-merito.it/web/trasparenza/papca-ap/-/papca/igrid/1074/770" >"${folder}"/../docs/c_i725/index.html
